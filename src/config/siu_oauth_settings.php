f24c276eeaa43e563c189a7563fc233ad4e4b4f3
aacotroneo <aacotroneo@gmail.com>
2015-02-09 17:49:39 +0000

    configuracion

---
+++ src/config/siu_oauth_settings.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return array(
+    'oauth2' => array(
+        'grant_type' => 'saml2',
+        'clientId' => 'arai_autogestion',
+        'clientSecret' => '123456',
+        'redirectUri' => 'http://localhost:8000/usuarios/backend/access_token',
+        'scopes' => array('cuenta_bancaria'),
+        'urlAuthorize' => 'http://localhost:8000/oauth/authorize',
+        'urlAccesToken' => 'http://localhost/oauth/token' //este va por canal seguro (sin :8000)
+    )
+);
